f99e1a64f4ebc4930b36f1a742bd54c7c18b3b02
Andrew Katz <andrew.katz@hey.com>
2025-10-02 14:25:01 -0700

    Updates

--- dot-config/nvim/lua/plugins/colors.lua
+++ dot-config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,21 @@
 return {
-  "EdenEast/nightfox.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme carbonfox]])
-  end,
+  {
+    "lilydjwg/colorizer",
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme carbonfox]])
+    end,
+  },
 }
+-- return {
+--   "dracula/vim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd([[colorscheme dracula]])
+--   end,
+-- }
